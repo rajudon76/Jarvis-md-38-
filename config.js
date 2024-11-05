69eb5987fd071c55be867254bee7445f215cf49f
rajudon76 <135225882+rajudon76@users.noreply.github.com>
2024-11-05 21:15:17 +0500

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ module.exports = {
   ANTIBOT_MSG: process.env.ANTIBOT_MSG || "_*Bot Not allowed in this group*_",
   ANTIWORD_MSG: process.env.ANTIWORD_MSG || "_*antiword deleted successfully*_",
   ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
-  SESSION_ID: process.env.SESSION_ID || 'Jarvis_cbed_c0be_1a5a_11a2_beff_5b59_1a9d_6be1',
+  SESSION_ID: process.env.SESSION_ID || 'Jarvis_cf13_0822_2895_8e3e_0729_9624_e0e3_5c2b',
   LANG: process.env.LANG || 'EN',
   ELEVENLABS: process.env.ELEVENLABS,
   HANDLERS: process.env.HANDLERS || process.env.HANDLER || process.env.PREFIX || '.',
